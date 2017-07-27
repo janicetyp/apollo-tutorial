42168d47705eabddda78d4818e82e122a67d3334
janicetan <janicetan@ip-192-168-108-133.ap-southeast-1.compute.internal>
2017-07-27 15:59:30 +0800

    Initial commit

---
+++ data/schema.js
@@ -0,0 +1,32 @@
+import {
+  makeExecutableSchema,
+  // addMockFunctionsToSchema,
+} from 'graphql-tools';
+import resolvers from './resolvers';
+// import mocks from './mocks';
+
+const typeDefs = `
+type Author {
+  id: Int
+  firstName: String
+  lastName: String
+  posts: [Post]
+}
+type Post {
+  id: Int
+  title: String
+  text: String
+  views: Int
+  author: Author
+}
+type Query {
+ author(firstName: String, lastName: String): Author
+ getFortuneCookie: String # we'll use this later
+},
+`;
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// addMockFunctionsToSchema({ schema, mocks });
+
+export default schema;
